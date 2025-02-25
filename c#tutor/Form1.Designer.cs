dfde24f8897269cc72c636a2a9814fb922c4f260
Programmer Pro1 <mtasfaye@gmail.com>
2025-02-25 09:06:08 +0300

    First commit

---
+++ c#tutor/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace c_tutor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtbi = new System.Windows.Forms.TextBox();
+            this.txtbun = new System.Windows.Forms.TextBox();
+            this.txtbup = new System.Windows.Forms.TextBox();
+            this.txtbur = new System.Windows.Forms.TextBox();
+            this.txtbr = new System.Windows.Forms.TextBox();
+            this.dtprd = new System.Windows.Forms.DateTimePicker();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.dataGridViewuserdata = new System.Windows.Forms.DataGridView();
+            this.btnrefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewuserdata)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "User name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 14);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "User password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(39, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 14);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "User role";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(39, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 14);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Register date";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(39, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 14);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Remark";
+            // 
+            // txtbi
+            // 
+            this.txtbi.Location = new System.Drawing.Point(137, 34);
+            this.txtbi.Name = "txtbi";
+            this.txtbi.Size = new System.Drawing.Size(273, 20);
+            this.txtbi.TabIndex = 6;
+            // 
+            // txtbun
+            // 
+            this.txtbun.Location = new System.Drawing.Point(137, 57);
+            this.txtbun.Name = "txtbun";
+            this.txtbun.Size = new System.Drawing.Size(273, 20);
+            this.txtbun.TabIndex = 7;
+            // 
+            // txtbup
+            // 
+            this.txtbup.Location = new System.Drawing.Point(137, 81);
+            this.txtbup.Name = "txtbup";
+            this.txtbup.Size = new System.Drawing.Size(273, 20);
+            this.txtbup.TabIndex = 8;
+            // 
+            // txtbur
+            // 
+            this.txtbur.Location = new System.Drawing.Point(137, 106);
+            this.txtbur.Name = "txtbur";
+            this.txtbur.Size = new System.Drawing.Size(273, 20);
+            this.txtbur.TabIndex = 9;
+            // 
+            // txtbr
+            // 
+            this.txtbr.Location = new System.Drawing.Point(137, 155);
+            this.txtbr.Multiline = true;
+            this.txtbr.Name = "txtbr";
+            this.txtbr.Size = new System.Drawing.Size(273, 72);
+            this.txtbr.TabIndex = 11;
+            // 
+            // dtprd
+            // 
+            this.dtprd.Location = new System.Drawing.Point(137, 130);
+            this.dtprd.Name = "dtprd";
+            this.dtprd.Size = new System.Drawing.Size(273, 20);
+            this.dtprd.TabIndex = 12;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnadd.Location = new System.Drawing.Point(137, 248);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(75, 28);
+            this.btnadd.TabIndex = 13;
+            this.btnadd.Text = "Add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnupdate.Location = new System.Drawing.Point(218, 248);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(75, 28);
+            this.btnupdate.TabIndex = 14;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.Location = new System.Drawing.Point(299, 248);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(75, 28);
+            this.btndelete.TabIndex = 15;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclear.Location = new System.Drawing.Point(380, 248);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 28);
+            this.btnclear.TabIndex = 16;
+            this.btnclear.Text = "Clear";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // dataGridViewuserdata
+            // 
+            this.dataGridViewuserdata.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridViewuserdata.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewuserdata.Location = new System.Drawing.Point(428, 34);
+            this.dataGridViewuserdata.Name = "dataGridViewuserdata";
+            this.dataGridViewuserdata.Size = new System.Drawing.Size(656, 193);
+            this.dataGridViewuserdata.TabIndex = 17;
+            this.dataGridViewuserdata.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewuserdata_CellContentClick);
+            // 
+            // btnrefresh
+            // 
+            this.btnrefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnrefresh.Location = new System.Drawing.Point(461, 248);
+            this.btnrefresh.Name = "btnrefresh";
+            this.btnrefresh.Size = new System.Drawing.Size(75, 28);
+            this.btnrefresh.TabIndex = 18;
+            this.btnrefresh.Text = "Refresh";
+            this.btnrefresh.UseVisualStyleBackColor = true;
+            this.btnrefresh.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1096, 502);
+            this.Controls.Add(this.btnrefresh);
+            this.Controls.Add(this.dataGridViewuserdata);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.dtprd);
+            this.Controls.Add(this.txtbr);
+            this.Controls.Add(this.txtbur);
+            this.Controls.Add(this.txtbup);
+            this.Controls.Add(this.txtbun);
+            this.Controls.Add(this.txtbi);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "C# Tutor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewuserdata)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtbi;
+        private System.Windows.Forms.TextBox txtbun;
+        private System.Windows.Forms.TextBox txtbup;
+        private System.Windows.Forms.TextBox txtbur;
+        private System.Windows.Forms.TextBox txtbr;
+        private System.Windows.Forms.DateTimePicker dtprd;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.DataGridView dataGridViewuserdata;
+        private System.Windows.Forms.Button btnrefresh;
+    }
+}
+
---
+++ c#tutor/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace c_tutor
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection con = new SqlConnection("Server=DESKTOP-QKNE475;Database=hr; Integrated Security=true");
+        public Form1()
+        { 
+            InitializeComponent();
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+
+            //con.Open();
+            //check validation 
+            if (txtbi.Text == "" || txtbun.Text == "" || txtbup.Text == "" || txtbur.Text == "" || dtprd.Text == "" || txtbr.Text == "")
+                MessageBox.Show("Complete the form first");
+            else
+                //check for duplication
+            con.Open();
+            SqlCommand sqlcmd = new SqlCommand("select * from usertbl where id=@uid", con);
+            sqlcmd.Parameters.AddWithValue("@uid", txtbi.Text);
+            SqlDataReader sqldr = sqlcmd.ExecuteReader(); 
+            if (sqldr.HasRows)
+            {
+                MessageBox.Show("User id already exist please try again!");
+
+            }
+            else
+            {
+                 sqldr.Close(); 
+                int status = 1;
+                SqlCommand sqlcm = new SqlCommand("insert into usertbl values('" + txtbi.Text + "','" + txtbun.Text + "','" + txtbup.Text + "','" + txtbur.Text + "','" + dtprd.Value.ToShortDateString() + "','" + txtbur.Text + "', '" + status + "')", con);
+                sqlcm.ExecuteNonQuery();
+                MessageBox.Show("Data Successfully Added", "Data Added", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtbi.Text = "";
+                txtbun.Text = "";
+                txtbup.Text = "";
+                txtbur.Text = "";
+                txtbr.Text = "";
+                txtbi.Focus();
+            }
+            con.Close(); 
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //view user data from usertbl during Form1_Load
+            viwdata("");
+        }
+        #region "view user data from usertbl"
+        public void viwdata(string svd)
+        {
+            try
+            {
+                String stv = ("select * from usertbl where concat(id,username) like '%" + svd + "%' and status = 1");
+                SqlCommand sqlcd = new SqlCommand(stv, con);
+                SqlDataAdapter sqlda = new SqlDataAdapter(sqlcd);
+                DataTable dataTable = new DataTable();
+                sqlda.Fill(dataTable);
+                dataGridViewuserdata.DataSource = dataTable;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error try again");
+            }
+        }
+        #endregion
+        #region "Update user data"
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            //check validation 
+            if (txtbi.Text == "")
+                MessageBox.Show("Select user data from datagridview first");
+            else
+             //check for user data availablity
+                con.Open();
+            SqlCommand sqlcmd = new SqlCommand("select * from usertbl where id=@uid", con);
+            sqlcmd.Parameters.AddWithValue("@uid", txtbi.Text);
+            SqlDataReader sqldr = sqlcmd.ExecuteReader();
+            if (sqldr.HasRows)
+            {
+                //update user data
+                sqldr.Close();
+                SqlCommand sqlcup = new SqlCommand("update usertbl set username='"+ txtbun.Text +"',userrole='"+ txtbur.Text +"', registerdate='"+ dtprd.Value.ToShortDateString()+"', remark='"+ txtbr.Text +"' where id='"+txtbi.Text+"'",con);
+                sqlcup.ExecuteNonQuery();
+                MessageBox.Show("Successfully Updated","Data Updated",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                //clear the form
+                txtbi.Text = "";
+                txtbun.Text = "";
+                txtbup.Text = "";
+                txtbur.Text = "";
+                txtbr.Text = "";
+                txtbi.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Please try again");
+            }
+            con.Close();
+        }
+
+    private void dataGridViewuserdata_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                int index = e.RowIndex;
+                DataGridViewRow selectedrow;
+                selectedrow = dataGridViewuserdata.Rows[index];
+                //0 to 5 indicate the index of field in user table
+                txtbi.Text = selectedrow.Cells[0].Value.ToString();
+                txtbun.Text=selectedrow.Cells[1].Value.ToString();
+                txtbup.Text = selectedrow.Cells[2].Value.ToString();
+                txtbur.Text = selectedrow.Cells[3].Value.ToString();
+                dtprd.Text = selectedrow.Cells[4].Value.ToString();
+                txtbr.Text = selectedrow.Cells[5].Value.ToString();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Error please try again");
+            }
+        }
+        #endregion
+ 
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            //check feild
+            if (txtbi.Text == "")
+            {
+                MessageBox.Show("Enter user id to delete");
+            }
+            //confirm delete
+            else if (MessageBox.Show("Confirm to delete ?", "Data Deleted", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("delete from usertbl where id = '" + txtbi.Text + "'", con);
+                //check as data exist
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    MessageBox.Show("User data successfully deleted");
+                }
+                else
+                {
+                    MessageBox.Show("No such user id exist!");
+                }
+                con.Close();
+            }
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            txtbi.Text = "";
+            txtbun.Text = "";
+            txtbup.Text = "";
+            txtbur.Text = "";
+            txtbr.Text = "";
+            txtbi.Focus();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Controls.Clear();
+            InitializeComponent();
+            Form1_Load(e, e);
+            Refresh();
+        }
+    }
+   }
+
